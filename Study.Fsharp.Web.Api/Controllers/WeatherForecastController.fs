62d09d13db6cce501f9e23678dc8e7d290c024d5
Felipe Segade <felipe.segade@outlook.com>
2025-11-05 09:44:48 +0100

    Initial commit

---
+++ Study.Fsharp.Web.Api/Controllers/WeatherForecastController.fs
@@ -0,0 +1,31 @@
+﻿namespace Study.Fsharp.Web.Api.Controllers
+
+open System
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open Study.Fsharp.Web.Api
+
+[<ApiController>]
+[<Route("[controller]")>]
+type WeatherForecastController(logger: ILogger<WeatherForecastController>) =
+    inherit ControllerBase()
+
+    let summaries =
+        [| "Freezing"
+           "Bracing"
+           "Chilly"
+           "Cool"
+           "Mild"
+           "Warm"
+           "Balmy"
+           "Hot"
+           "Sweltering"
+           "Scorching" |]
+
+    [<HttpGet>]
+    member _.Get() =
+        let rng = System.Random()
+        [| for index in 0..4 ->
+               { Date = DateTime.Now.AddDays(float index)
+                 TemperatureC = rng.Next(-20, 55)
+                 Summary = summaries.[rng.Next(summaries.Length)] } |]
---
+++ Study.Fsharp.Web.Api/Program.fs
@@ -0,0 +1,41 @@
+namespace Study.Fsharp.Web.Api
+
+#nowarn "20"
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Scalar.AspNetCore
+open Serilog
+
+module Program =
+    let exitCode = 0
+
+    [<EntryPoint>]
+    let main args =
+
+        let builder = WebApplication.CreateBuilder(args)
+
+        builder.Services.AddControllers()
+
+        builder.Services.AddOpenApi()
+
+        builder.Services.AddSerilog(fun loggerConfiguration ->
+            loggerConfiguration.ReadFrom.Configuration(builder.Configuration) |> ignore)
+
+        let app = builder.Build()
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        app.MapOpenApi()
+        app.MapScalarApiReference()
+
+        app.UseSerilogRequestLogging(fun configureOptions ->
+            configureOptions.IncludeQueryInRequestPath = true |> ignore)
+
+        app.Run()
+
+        exitCode
